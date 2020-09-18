3c46bc1462d4d4b546fabeae6b6633b043c4db55
Tanner Stephens <tannerrstephens@gmail.com>
2020-09-18 00:33:37 -0500

    Update gh-pages

--- projectSettings.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  title: 'Mandel-JS',
-  repo: 'https://github.com/tannerstephens/mandel-js'
-};
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import App from './app';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new App();
-  app.display();
-})
